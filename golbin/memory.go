04d52210581ac8f7dcc9b783d0ff1f302243d51d
AbhishekKr <abhikumar163@gmail.com>
2014-03-05 23:14:10 +0530

    [golbin] hardcoded MemInfo field made parameteric

--- golbin/memory.go
+++ golbin/memory.go
@@ -1,13 +1,14 @@
 package golbin
 
 import (
+  "fmt"
   "strings"
 )
 
 func MemInfo(field string) string{
   kon := Console{Command: "cat /proc/meminfo"}
   kon.Run()
-  kon = Console{Command: "grep MemFree", StdInput: kon.StdOutput}
+  kon = Console{Command: fmt.Sprintf("grep %s", field), StdInput: kon.StdOutput}
   kon.Run()
   return strings.Fields(kon.StdOutput)[1]
 }
