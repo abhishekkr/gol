2c4c78244ce0add8084050825b54ae773c6cc68e
AbhishekKr <abhikumar163@gmail.com>
2014-06-22 18:14:28 +0530

    [goltime] TimestampNow to return Timestamp for Time.now

--- examples/goltime/from_time_points.go
+++ examples/goltime/from_time_points.go
@@ -1,26 +1,26 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
 
-  goltime "github.com/abhishekkr/gol/goltime"
+	goltime "github.com/abhishekkr/gol/goltime"
 )
 
+func main() {
+	timestamp01 := goltime.Timestamp{
+		Year:  2014,
+		Month: 3,
+		Day:   8,
+		Hour:  9,
+		Min:   44,
+		Sec:   55,
+	}
 
-func main(){
-  timestamp01 := goltime.Timestamp{
-    Year: 2014,
-    Month: 3,
-    Day: 8,
-    Hour: 9,
-    Min: 44,
-    Sec: 55,
-  }
+	timestamp02 := goltime.CreateTimestamp([]string{
+		"2014", "3", "8", "9", "45", "56",
+	})
 
-  timestamp02 := goltime.CreateTimestamp([]string{
-    "2014", "3", "8", "9", "45", "56",
-  })
-
-  fmt.Println(timestamp01.Time())
-  fmt.Println(timestamp02.Time())
+	fmt.Println(timestamp01.Time())
+	fmt.Println(timestamp02.Time())
+	fmt.Println(goltime.TimestampNow())
 }
--- goltime/from_time_points.go
+++ goltime/from_time_points.go
@@ -6,10 +6,12 @@ import (
 	"time"
 )
 
+// the currency for goltime
 type Timestamp struct {
 	Year, Month, Day, Hour, Min, Sec int
 }
 
+// creating Timestamp struct from string array of counts [Y M D h m s]
 func CreateTimestamp(time_point []string) Timestamp {
 	year, _ := strconv.Atoi(time_point[0])
 	month, _ := strconv.Atoi(time_point[1])
@@ -28,6 +30,10 @@ func CreateTimestamp(time_point []string) Timestamp {
 	}
 }
 
+/*
+creating Timestamp from Request handler of HTTP having formvalues for it
+under "year", "month", "day", "hour", "min", "sec"
+*/
 func TimestampFromHTTPRequest(req *http.Request) Timestamp {
 	return CreateTimestamp([]string{
 		req.FormValue("year"), req.FormValue("month"), req.FormValue("day"),
@@ -35,7 +41,24 @@ func TimestampFromHTTPRequest(req *http.Request) Timestamp {
 	})
 }
 
+// create time.Time from a Timestamp struct
 func (timestamp *Timestamp) Time() time.Time {
 	return time.Date(timestamp.Year, time.Month(timestamp.Month), timestamp.Day,
 		timestamp.Hour, timestamp.Min, timestamp.Sec, 0, time.UTC)
+}
+
+// create Timestamp struct from time.Now
+func TimestampNow() Timestamp {
+	var year, day, hour, min, sec int
+	var month time.Month
+	year, month, day = time.Now().Date()
+	hour, min, sec = time.Now().Clock()
+	return Timestamp{
+		Year:  year,
+		Month: int(month),
+		Day:   day,
+		Hour:  hour,
+		Min:   min,
+		Sec:   sec,
+	}
 }
