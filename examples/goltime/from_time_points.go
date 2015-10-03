6452c6118cd06728eaa32e888c7b195c2ba2afea
AbhishekKr <abhikumar163@gmail.com>
2015-10-03 15:59:26 +0530

    [goltime] added new example

--- examples/goltime/from_time_points.go
+++ examples/goltime/from_time_points.go
@@ -23,4 +23,14 @@ func main() {
 	fmt.Println(timestamp01.Time())
 	fmt.Println(timestamp02.Time())
 	fmt.Println(goltime.TimestampNow())
+
+	timestamp03 := goltime.CreateTimestamp([]string{
+		"2014", "3", "8", "9", "45", "56.0.0.100",
+	})
+	fmt.Println(timestamp03.Time())
+
+	timestamp04 := goltime.CreateTimestamp([]string{
+		"2014", "3", "8", "9", "45", "56.0.0.200",
+	})
+	fmt.Println(timestamp04)
 }
