7dfbdc6385d9f354ee99ce5f0e3839dd6fb44d7b
AbhishekKr <abhikumar163@gmail.com>
2017-03-01 01:08:33 +0530

    [golenv] some env variable helper
    Signed-off-by: AbhishekKr <abhikumar163@gmail.com>

---
+++ golenv/env.go
@@ -0,0 +1,34 @@
+package golenv
+
+import (
+	"os"
+	"strings"
+)
+
+func OverrideIfEnv(envVar string, defaultValue string) string {
+	if HasEnv(envVar) {
+		envVarValue := os.Getenv(envVar)
+		return envVarValue
+	}
+	return defaultValue
+}
+
+func HasEnv(envVar string) bool {
+	for _, envKeyVal := range os.Environ() {
+		if strings.Split(envKeyVal, "=")[0] == envVar {
+			return true
+		}
+	}
+	return false
+}
+
+func EnvMap() map[string]string {
+	var key_val map[string]string
+	key_val = make(map[string]string)
+
+	for _, envKeyVal := range os.Environ() {
+		envKeyValSplit := strings.Split(envKeyVal, "=")
+		key_val[envKeyValSplit[0]] = envKeyValSplit[1]
+	}
+	return
+}
