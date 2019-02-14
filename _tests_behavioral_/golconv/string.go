07d83a7fc32c9d1894bedba4d15beb64851f5574
AbhishekKr <abhikumar163@gmail.com>
2019-02-15 00:52:48 +0530

    [golconv] added StringToInt for conversion with default value
    Signed-off-by: AbhishekKr <abhikumar163@gmail.com>

--- _tests_behavioral_/golconv/string.go
+++ _tests_behavioral_/golconv/string.go
@@ -1,8 +1,12 @@
 package main
 
-import "github.com/abhishekkr/gol/golconv"
+import (
+	"fmt"
 
-func main() {
+	"github.com/abhishekkr/gol/golconv"
+)
+
+func testStringToUint64() {
 	u := golconv.StringToUint64("100", 10)
 	if u != uint64(100) {
 		panic("StringToUint64 for correct string failed")
@@ -17,4 +21,27 @@ func main() {
 	if u != uint64(10) {
 		panic("StringToUint64 for wrong string failed")
 	}
+}
+
+func testStringToInt() {
+	i := golconv.StringToInt("100", 10)
+	if i != 100 {
+		panic("StringToInt for correct string failed")
+	}
+
+	i = golconv.StringToInt("00", 10)
+	if i != 0 {
+		panic("StringToInt for correct string failed")
+	}
+
+	i = golconv.StringToInt("i00", 10)
+	if i != 10 {
+		panic("StringToInt for wrong string failed")
+	}
+}
+
+func main() {
+	testStringToUint64()
+	testStringToInt()
+	fmt.Println("+ golconv passed")
 }
--- golconv/string.go
+++ golconv/string.go
@@ -2,6 +2,14 @@ package golconv
 
 import "strconv"
 
+func StringToInt(n string, defaultValue int) int {
+	val, err := strconv.Atoi(n)
+	if err == nil {
+		return val
+	}
+	return defaultValue
+}
+
 func StringToUint64(n string, defaultValue uint64) uint64 {
 	val, err := strconv.Atoi(n)
 	if err == nil {
